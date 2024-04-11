4044118862a37d2cd5f84fd49c8e93cfec3c21bc
Tori Engler <toriengler2@gmail.com>
2024-04-11 18:20:15 +0000

    added html for table

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -9,17 +9,10 @@ $(function () {
         $("#wishlist_id").val(res.id);
         $("#wishlist_title").val(res.title);
         $("#wishlist_user_id").val(res.user_id);
-        // $("#wishlist_count").val(res.count);
+        $("#wishlist_count").val(res.count);
         $("#wishlist_description").val(res.description);
         $("#wishlist_items").val(res.items);
         $("#wishlist_date").val(res.date);
-        // if (res.available == true) {
-        //     $("#wishlist_available").val("true");
-        // } else {
-        //     $("#wishlist_available").val("false");
-        // }
-        // $("#wishlist_gender").val(res.gender);
-        // $("#pet_birthday").val(res.birthday);
     }
 
     /// Clears all form fields
@@ -28,7 +21,7 @@ $(function () {
         $("#wishlist_description").val("");
         $("#wishlist_date").val("");
         $("#wishlist_user_id").val("");
-       // $("#wishlist_count").val("");
+        $("#wishlist_count").val("");
         $("#wishlist_items").val("");
     }
 
@@ -46,7 +39,7 @@ $(function () {
 
         let title = $("#wishlist_title").val();
         let description = $("#wishlist_description").val();
-       // let count = $("#wishlist_count").val() == "true";
+        let count = $("#wishlist_count").val();
         let user_id = $("#wishlist_user_id").val();
         let items = $("#wishlist_items").val();
         let date = $("#wishlist_date").val();
@@ -55,7 +48,7 @@ $(function () {
             "title": title,
             "description": description,
             "items": items,
-          //  "count": count,
+            "count": count,
             "user_id": user_id,
             "date": date
         };
@@ -89,7 +82,7 @@ $(function () {
         let wishlist_id = $("#wishlist_id").val();
         let title = $("#wishlist_title").val();
         let description = $("#wishlist_description").val();
-       // let count = $("#wishlist_count").val() == "true";
+        let count = $("#wishlist_count").val();
         let user_id = $("#wishlist_user_id").val();
         let items = $("#wishlist_items").val();
         let date = $("#wishlist_date").val();
@@ -98,7 +91,7 @@ $(function () {
             "title": title,
             "description": description,
             "items": items,
-        //    "count": count,
+           "count": count,
             "user_id": user_id,
             "date": date
         };
@@ -184,86 +177,84 @@ $(function () {
 //     // Clear the form
 //     // ****************************************
 
-//     $("#clear-btn").click(function () {
-//         $("#pet_id").val("");
-//         $("#flash_message").empty();
-//         clear_form_data()
-//     });
+    $("#clear-btn").click(function () {
+        $("#wishlist_id").val("");
+        $("#flash_message").empty();
+        clear_form_data()
+    });
 
 //     // ****************************************
 //     // Search for a Pet
 //     // ****************************************
 
-//     $("#search-btn").click(function () {
+    $("#search-btn").click(function () {
 
-//         let name = $("#pet_name").val();
-//         let category = $("#pet_category").val();
-//         let available = $("#pet_available").val() == "true";
+        let title = $("#wishlist_title").val();
+        let wishlist_id = $("#wishlist_id").val();
+        //let user_id = $("#user_id").val();
 
-//         let queryString = ""
+        let queryString = ""
 
-//         if (name) {
-//             queryString += 'name=' + name
-//         }
-//         if (category) {
-//             if (queryString.length > 0) {
-//                 queryString += '&category=' + category
-//             } else {
-//                 queryString += 'category=' + category
-//             }
-//         }
-//         if (available) {
-//             if (queryString.length > 0) {
-//                 queryString += '&available=' + available
-//             } else {
-//                 queryString += 'available=' + available
-//             }
-//         }
+        if (title) {
+            queryString += 'title=' + title
+        }
+        if (wishlist_id) {
+            queryString += 'id=' + category
+             
+        }
+        // if (available) {
+        //     if (queryString.length > 0) {
+        //         queryString += '&available=' + available
+        //     } else {
+        //         queryString += 'available=' + available
+        //     }
+        // }
 
-//         $("#flash_message").empty();
+        $("#flash_message").empty();
 
-//         let ajax = $.ajax({
-//             type: "GET",
-//             url: `/pets?${queryString}`,
-//             contentType: "application/json",
-//             data: ''
-//         })
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/wishlists?${queryString}`,
+            contentType: "application/json",
+            data: ''
+        })
 
-//         ajax.done(function(res){
-//             //alert(res.toSource())
-//             $("#search_results").empty();
-//             let table = '<table class="table table-striped" cellpadding="10">'
-//             table += '<thead><tr>'
-//             table += '<th class="col-md-2">ID</th>'
-//             table += '<th class="col-md-2">Name</th>'
-//             table += '<th class="col-md-2">Category</th>'
-//             table += '<th class="col-md-2">Available</th>'
-//             table += '<th class="col-md-2">Gender</th>'
-//             table += '<th class="col-md-2">Birthday</th>'
-//             table += '</tr></thead><tbody>'
-//             let firstPet = "";
-//             for(let i = 0; i < res.length; i++) {
-//                 let pet = res[i];
-//                 table +=  `<tr id="row_${i}"><td>${pet.id}</td><td>${pet.name}</td><td>${pet.category}</td><td>${pet.available}</td><td>${pet.gender}</td><td>${pet.birthday}</td></tr>`;
-//                 if (i == 0) {
-//                     firstPet = pet;
-//                 }
-//             }
-//             table += '</tbody></table>';
-//             $("#search_results").append(table);
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-2">ID</th>'
+            table += '<th class="col-md-2">Title</th>'
+            table += '<th class="col-md-2">Description</th>'
+            table += '<th class="col-md-2">Items</th>'
+            table += '<th class="col-md-2">Count</th>'
+            table += '<th class="col-md-2">Date</th>'
+            table += '<th class="col-md-2">User ID</th>'
+            table += '</tr></thead><tbody>'
+            let firstWList = "";
+            for(let i = 0; i < res.length; i++) {
+                let wishlist = res[i];
+                table +=  `<tr id="row_${i}"><td>${wishlist.id}</td><td>${wishlist.title}</td><td>${wishlist.description}</td><td>${wishlist.items}</td><td>${wishlist.count}</td><td>${wishlist.date}</td></tr>`;
+                if (i == 0) {
+                    firstWList = wishlist;
+                }
+            }
+            table += '</tbody></table>';
+            $("#search_results").append(table);
 
-//             // copy the first result to the form
-//             if (firstPet != "") {
-//                 update_form_data(firstPet)
-//             }
+            // copy the first result to the form
+            if (firstWList != "") {
+                update_form_data(firstWList)
+            }
 
-//             flash_message("Success")
-//         });
+            flash_message("Success")
+        });
 
-//         ajax.fail(function(res){
-//             flash_message(res.responseJSON.message)
-//         });
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
 
-//     });
+    });
 
  })
