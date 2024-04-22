d15a0d948d3832b4050121144fef3f36f0fefacf
Tori Engler <toriengler2@gmail.com>
2024-04-22 20:03:21 +0000

    updating datetime line

--- service/routes.py
+++ service/routes.py
@@ -20,11 +20,10 @@ Wishlist Store Service
 This service implements a REST API that allows you to Create, Read, Update
 and Delete Wishlist from the inventory of wishlists in the WishlistShop
 """
-
+from datetime import date
 from flask import jsonify, request, url_for, abort
 from flask import current_app as app  # Import Flask application
 from service.models.item import Item
-from datetime import date
 from service.models.wishlist import Wishlist
 from service.common import status  # HTTP Status Codes
 
